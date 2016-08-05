b81f748b278948ab08f1bcf26ce418aebbc483a3
Roy Hyunjin Han <rhh@crosscompute.com>
2016-08-05 00:00:17 -0400

    Add get_unique_instance

--- invisibleroads_records/models.py
+++ invisibleroads_records/models.py
@@ -1,11 +1,34 @@
+from invisibleroads_macros.security import make_random_string
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from zope.sqlalchemy import ZopeTransactionExtension
 
+from .exceptions import InvisibleRoadsRecordsError
 from .libraries.cache import CachingQuery
 
 
 db = scoped_session(sessionmaker(
     extension=ZopeTransactionExtension(), query_cls=CachingQuery))
 Base = declarative_base()
+
+
+def get_unique_instance(Class, id_length=16, retry_count=3):
+    count = 0
+    while count < retry_count:
+        instance = Class(id=make_random_string(id_length))
+        print instance.id
+        try:
+            db.add(instance)
+            db.flush()
+        except IntegrityError as e:
+            print e
+            db.rollback()
+        else:
+            break
+        count += 1
+    else:
+        raise InvisibleRoadsRecordsError(
+            'could not get unique instance (%s)' % Class)
+    return instance
