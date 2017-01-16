a01983706ad764986d3c1b4982c7c0dbf70e39c2
Roy Hyunjin Han <rhh@crosscompute.com>
2017-01-16 15:02:18 +0000

    Restore pytest-runner

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     setup_requires=[
-        'pytest-runner',
+        'pytest-runner'
     ],
     install_requires=[
         'pyramid-tm',
