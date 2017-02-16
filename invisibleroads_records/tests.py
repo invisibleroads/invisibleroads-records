841c9d3da22f6cecabe3c4d438296e1a764e0dcd
Roy Hyunjin Han <rhh@crosscompute.com>
2017-02-16 06:58:35 -0500

    Fix tests

--- invisibleroads_records/tests.py
+++ invisibleroads_records/tests.py
@@ -1,30 +1,36 @@
 import transaction
 from pytest import fixture
 from sqlalchemy import engine_from_config
+from webtest import TestApp
 
+from invisibleroads_records import main as get_app
 from invisibleroads_records.models import Base, get_database_session
 
 
 @fixture
-def records_request(posts_request, config, database):
-    config.include('invisibleroads_records')
-    records_request = posts_request
-    records_request.database = database
-    yield records_request
+def records_website(records_request):
+    settings = records_request.registry.settings
+    yield TestApp(get_app({}, **settings))
 
 
 @fixture
-def database(database_engine):
-    yield get_database_session(database_engine, transaction.manager)
-    transaction.abort()
-
-
-@fixture
-def database_engine(settings):
+def records_request(posts_request, website_config):
+    settings = website_config.registry.settings
     database_engine = engine_from_config(settings)
     Base.metadata.create_all(database_engine)
-    yield database_engine
+    records_request = posts_request
+    records_request.database = get_database_session(
+        database_engine, transaction.manager)
+    yield records_request
+    transaction.abort()
     Base.metadata.drop_all(database_engine)
+
+
+@fixture
+def website_config(config):
+    config.include('invisibleroads_posts')
+    config.include('invisibleroads_records')
+    yield config
 
 
 @fixture
