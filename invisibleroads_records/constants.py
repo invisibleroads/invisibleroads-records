322e4aebe7bca55f72fb0098fa40d4de8ace49d6
Roy Hyunjin Han <rhh@crosscompute.com>
2020-12-14 13:52:16 -0500

    Increase default id length

--- invisibleroads_records/constants.py
+++ invisibleroads_records/constants.py
@@ -5,5 +5,5 @@ NAMING_CONVENTION = {
     'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
     'pk': 'pk_%(table_name)s',
 }
-RECORD_ID_LENGTH = 8
+RECORD_ID_LENGTH = 16
 RECORD_RETRY_COUNT = 3
